930c0aa9f8f95d8dfb4d791b15211f85e6d6f8a3
cybersufi <cybersufi@gmail.com>
2014-02-28 10:31:18 +0700

    db added
    db added

---
+++ db/localhost.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.9
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Feb 28, 2014 at 04:29 AM
+-- Server version: 5.6.14
+-- PHP Version: 5.5.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `portal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `app_sessions`
+--
+
+CREATE TABLE IF NOT EXISTS `app_sessions` (
+  `session_id` varchar(40) DEFAULT '',
+  `ip_address` varchar(16) DEFAULT '',
+  `user_agent` varchar(50) DEFAULT NULL,
+  `last_activity` int(10) unsigned DEFAULT '0',
+  `user_data` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
